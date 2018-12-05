cec931208f5257e50c074d0104d3e6b4f81abff6
JeanAnx <jean.annaix@gmail.com>
2018-12-05 16:59:47 +0100

    new commit

--- src/POE/Dungeon.php
+++ src/POE/Dungeon.php
@@ -2,10 +2,32 @@
 
 namespace POE;
 
+use POE\database\CharacterLoader;
+
 class Dungeon
 {
     public function reportSituation()
     {
-        return 'Le jeu n\'a pas encore commencé';
+        /**
+         * On passe par un objet intermédiaire pour récupérer notre personnage
+         * par anticipation avec le fait qu'il viendra de la base de données
+         */
+        $loader = new CharacterLoader();
+        $character = $loader->load();
+        
+        /**
+         * Démarrage d'un tampon de sortie / Output Buffet
+         * Dans cette "zone" tampon, le html généré par le fichier inclus sera stocké
+         * sans partir directement vers le serveur HTTP
+         */
+        ob_start();
+        include __DIR__ . '/../../template/reportSituation.html.php';
+        $output = ob_get_clean();
+        /**
+         * Après avoir écrit le document (capturé dans le tampon de sortie ),
+         * on décide de le faire redescendre dans une variable PHP et on nettoie
+         * (vide + désactive) le système de tampon
+         */
+        return $output;
     }
 }
---
+++ src/POE/database/Character.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace POE\database;
+
+class Character
+{
+    public function getName()
+    {
+        return 'Captain Perigord';
+    }
+    
+}
